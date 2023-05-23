fa8b2a9320fa4f6c5397504e2ba282711a5dc8c0
erixweb <erikmorenodev@gmail.com>
2023-05-23 21:53:26 +0200

    first commit

---
+++ src/components/App.jsx
@@ -0,0 +1,13 @@
+import Header from "./Header";
+import "../styles/index.css"
+import FeaturesCard from "./FeaturesCard";
+export default function App() {
+    return (
+        <>
+            <Header />
+            <main>
+                <FeaturesCard />
+            </main>
+        </>
+    )
+}
---
+++ src/components/Header.jsx
@@ -0,0 +1,17 @@
+export default function Header () {
+    return (
+        <header>
+            <div>
+                <h1>
+                    ReactDeploy
+                </h1>
+                <p>
+                    Despliega tu aplicación en pocos segundos empezando por 100gb/mes gratuitamente.
+                </p>
+            </div>
+            <div>
+                <img src="/assets/react.svg" alt="React logo" />
+            </div>
+        </header>
+    )
+}
