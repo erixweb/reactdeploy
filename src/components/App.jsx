5d31a4ff30a2eafb67611d0281d660986323ba34
erixweb <erikmorenodev@gmail.com>
2023-05-27 16:43:10 +0200

    Compatible section

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,12 +1,14 @@
 import Header from "./Header";
 import "../styles/index.css"
 import FeaturesCards from "./FeatureCards";
+import Technologies from "./Technologies";
 export default function App() {
     return (
         <>
             <Header />
             <main>
                 <FeaturesCards />
+                <Technologies />
             </main>
         </>
     )
---
+++ src/components/Technologies.jsx
@@ -0,0 +1,15 @@
+export default function Technologies () {
+    return (
+        <section id="technologies">
+            <h3>
+                Compatible con lo que usas
+            </h3>
+            <div>
+                <img src="/assets/vite.svg" alt="Imagen de vite" />
+                <img src="/assets/nextjs.svg" alt="Imagen de vite" />
+                <img src="/assets/reactblack.svg" alt="Imagen de vite" />
+                <img src="/assets/remix.svg" alt="Imagen de vite" />
+            </div>
+        </section>
+    )
+}
