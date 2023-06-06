55eb501f9f39389ac6b76f989a2a2a32276859c7
erixweb <erikmorenodev@gmail.com>
2023-06-06 17:44:30 +0200

    Bg to header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,16 +1,18 @@
 export default function Header () {
     return (
         <header>
-            <div>
-                <h1>
-                    ReactDeploy
-                </h1>
-                <p>
-                    Despliega tu aplicación en pocos segundos empezando por 100gb/mes gratuitamente.
-                </p>
-            </div>
-            <div>
-                <img src="/assets/react.svg" alt="React logo" />
+            <div className="wrap">
+                <div className="info">
+                    <h1>
+                        ReactDeploy
+                    </h1>
+                    <p>
+                        Despliega tu aplicación en pocos segundos empezando por 100gb/mes gratuitamente.
+                    </p>
+                </div>
+                <div className="logo">
+                    <img src="/assets/react.svg" alt="React logo" />
+                </div>
             </div>
         </header>
     )
